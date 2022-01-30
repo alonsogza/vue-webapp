c036040578bd8adb41507ce217981f2b82aa0914
alonsog <alonso_03@hotmail.com>
2022-01-30 00:15:58 -0600

    feat:: Se corrige los botones de Atras-Siguiente en la lista de pokemons, falta solucionar la recarga de esta pagina cuando regresa de mostrar la imagen de algun pokemon

--- src/components/RestaruanteList.vue
+++ src/components/RestaruanteList.vue
@@ -2,16 +2,26 @@
     <div>
         <h2>Esto es la {{texto}}</h2>
         <p class="text-start fw-bold">{{msg}}</p>
-        <button v-on:click="siguiente += 20; getPokemones(siguiente)" class="btn btn-primary">Siguiente</button>
-        <button v-on:click="atras -= 20; getPokemones(atras)" class="btn btn-primary">Atras</button>
-        <ul class="list-group">
-            <li v-for="item in infoLista" :key="item.name" class="list-group-item">
-                <!-- <a v-bind:href=item.url class="btn btn-primary"
-                    target="_blank">{{ item.name }}
-                </a> -->
-                <router-link :to="{name:'imagen-pokemon', params:{id: item.name}}"> {{ item.name }} </router-link> <!-- Se agrega una nueva ruta para mostrar la imagen-->
-            </li>
-        </ul>
+        <div class="row justify-content-between">
+            <div class="col-4">
+                <button v-on:click="siguiente_bloque -= 20; getPokemones(siguiente_bloque)" class="btn btn-outline-primary">Atras</button>
+            </div>
+            <div class="col-4">
+                <button v-on:click="siguiente_bloque += 20; getPokemones(siguiente_bloque)" class="btn btn-outline-primary">Siguiente</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul class="list-group">
+                    <li v-for="item in infoLista" :key="item.name" class="list-group-item">
+                        <!-- <a v-bind:href=item.url class="btn btn-primary"
+                            target="_blank">{{ item.name }}
+                        </a> -->
+                        <router-link :to="{name:'imagen-pokemon', params:{id: item.name}}"> {{ item.name }} </router-link> <!-- Se agrega una nueva ruta para mostrar la imagen-->
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -30,8 +40,7 @@ import axios from 'axios';
                 texto: 'Página Restaruantes LIST',
                 msg: 'Lista de Pokemones',
                 infoLista: [], // Agegamos el arreglo donde se almacena la respuesta de la API
-                siguiente: 0,
-                atras: 0,
+                siguiente_bloque: 0,
             }
         },
         // Creamos un METODO para usar AXIOS
